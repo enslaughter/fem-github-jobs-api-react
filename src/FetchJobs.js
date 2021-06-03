fcd9dd75b4f8b62adad2885bfe95f1e4734f2f36
E. N. Slaughter <enslaugh@gmail.com>
2021-06-03 10:25:52 -0400

    RIP GitHub Jobs

--- src/FetchJobs.js
+++ src/FetchJobs.js
@@ -1,55 +1,58 @@
-import { useEffect, useState } from 'react';
-import './styles/App.scss';
+import { useEffect, useState } from "react";
+import "./styles/App.scss";
 import ListingCard from "./components/ListingCard";
-import {snakeCase} from "lodash";
+import { snakeCase } from "lodash";
 
 function FetchJobs(props) {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [moreLoaded, setMoreLoaded] = useState(false);
+  const [moreError, setMoreError] = useState(null);
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [canLoadMore, setCanLoadMore] = useState(false);
 
-  const[error, setError] = useState(null);
-  const[isLoaded, setIsLoaded] = useState(false);
-  const[moreLoaded, setMoreLoaded] = useState(false);
-  const[moreError, setMoreError] = useState(null);
-  const[items, setItems] = useState([]);
-  const[currentPage, setCurrentPage] = useState(1);
-  const[canLoadMore, setCanLoadMore] = useState(false);
-
-  function generateQuery(filters, pagenum = 0){
+  function generateQuery(filters, pagenum = 0) {
     let query = `${process.env.REACT_APP_CORS_PROXY}https://jobs.github.com/positions.json`;
     let queryPrefixes = 0;
 
-    if(pagenum!==0){
+    if (pagenum !== 0) {
       queryPrefixes++;
-      query+=`${queryPrefixes===1 ? "?" : "&"}page=${pagenum}`;
+      query += `${queryPrefixes === 1 ? "?" : "&"}page=${pagenum}`;
     }
 
-    if(filters.title!==""){
+    if (filters.title !== "") {
       queryPrefixes++;
-      query+= `${queryPrefixes===1 ? "?" : "&"}search=${snakeCase(filters.title)}`;
+      query += `${queryPrefixes === 1 ? "?" : "&"}search=${snakeCase(
+        filters.title
+      )}`;
     }
 
-    if(filters.location!==""){
+    if (filters.location !== "") {
       queryPrefixes++;
-      query+= `${queryPrefixes===1 ? "?" : "&"}location=${filters.location}`;
+      query += `${queryPrefixes === 1 ? "?" : "&"}location=${filters.location}`;
     }
 
-    if(filters.full_time){
+    if (filters.full_time) {
       queryPrefixes++;
-      query+= `${queryPrefixes===1 ? "?" : "&"}full_time=${filters.full_time}`;
+      query += `${queryPrefixes === 1 ? "?" : "&"}full_time=${
+        filters.full_time
+      }`;
     }
     //console.log("Generated query for: " + query);
     return query;
   }
-    
+
   useEffect(() => {
     //If the filters change, then reset the page state to 1:
     setCurrentPage(1);
     setCanLoadMore(true);
 
     fetch(generateQuery(props.filters))
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
-          if (result.length < 50){
+          if (result.length < 50) {
             setCanLoadMore(false);
           } else {
             setCanLoadMore(true);
@@ -63,65 +66,75 @@ function FetchJobs(props) {
           setIsLoaded(true);
           setError(err);
         }
-      )
-  }, [props.filters])
+      );
+  }, [props.filters]);
 
-function addJobs(pagenum){
-  fetch(generateQuery(props.filters, pagenum))
-      .then(res => res.json())
+  function addJobs(pagenum) {
+    fetch(generateQuery(props.filters, pagenum))
+      .then((res) => res.json())
       .then(
         (result) => {
           //console.log(result);
           setMoreLoaded(true);
-          if (result===[]){
-            setCanLoadMore(false)
+          if (result === []) {
+            setCanLoadMore(false);
           } else {
-            setItems(prevItems => ([
-              ...prevItems, ...result
-            ]));
+            setItems((prevItems) => [...prevItems, ...result]);
           }
-          
         },
 
         (err) => {
           setMoreLoaded(false);
           setMoreError(err);
         }
-      )
+      );
 
-  setMoreLoaded(false);
-}
+    setMoreLoaded(false);
+  }
 
-function handleLoadMore(){
-  setCurrentPage(currentPage + 1);
-  //console.log("Loading page: " + currentPage);
-  addJobs(currentPage);
-}
+  function handleLoadMore() {
+    setCurrentPage(currentPage + 1);
+    //console.log("Loading page: " + currentPage);
+    addJobs(currentPage);
+  }
 
   if (error) {
-    return<div><h1 style={{textAlign: "center"}}>
-    ERROR: {error.message}
-</h1></div>;
-  } else if (!isLoaded) {
-    return <div><h1 style={{textAlign: "center"}}>
-    Loading Listings...
-</h1></div>;
-  } else {
-    return (<div className="job-listing-container" data-theme={props.toggleState}>
-{items.map((item, id) => (
-          <ListingCard listing={item} key={id} toggleState={props.toggleState} />
-        ))}
-        <div className="loadmore-container">
-          {canLoadMore ? <button className="app-button loadmore-button" onClick={handleLoadMore}>Load More</button> : <p>All Results Loaded</p>}
-          {moreError && <p style={{textAlign: "center"}}>Error: {moreError.message}</p>}
-          {!moreLoaded && <p style={{textAlign: "center"}}>Loading more results...</p>}
-        </div>
-        </div>
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>ERROR: {error.message}</h1>
+      </div>
     );
-     
-        
+  } else if (!isLoaded) {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>Loading Listings...</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ margin: "0 10%" }}>
+        <h1 style={{ textAlign: "center" }}>GitHub Jobs is Deprecated...</h1>
+        <p>
+          Unforunately, GitHub Jobs is officially deprecated, and so is its API.
+          I'm currently working on a fix to use some static data for the pages
+          instead. However, the code should still very much be viewable and it
+          did function when the API was still running. I apologize for the
+          inconvenience.
+        </p>
+      </div>
+    );
+    //     return (<div className="job-listing-container" data-theme={props.toggleState}>
+    // {items.map((item, id) => (
+    //           <ListingCard listing={item} key={id} toggleState={props.toggleState} />
+    //         ))}
+    //         <div className="loadmore-container">
+    //           {canLoadMore ? <button className="app-button loadmore-button" onClick={handleLoadMore}>Load More</button> : <p>All Results Loaded</p>}
+    //           {moreError && <p style={{textAlign: "center"}}>Error: {moreError.message}</p>}
+    //           {!moreLoaded && <p style={{textAlign: "center"}}>Loading more results...</p>}
+    //         </div>
+    //         </div>
+    // );
   }
- 
 }
 
 export default FetchJobs;
